8be31947d290b9d7a7c369e797cded8bbf3f84e3
Hanga Andras-Letanovszky <hangaa@arizona.edu>
2025-04-03 01:53:37 -0700

    Uploaded Research Assignment 3

---
+++ ResearchAssignments/ResearchAssignment3/ContourFitting.py
@@ -0,0 +1,77 @@
+# Density contour plotting code from Lab 7
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.optimize as so
+
+def find_confidence_interval(x, pdf, confidence_level):
+    return pdf[pdf > x].sum() - confidence_level
+
+def density_contour(xdata, ydata, nbins_x, nbins_y, ax=None, **contour_kwargs):
+    """ Create a density contour plot.
+    Parameters
+    ----------
+    xdata : numpy.ndarray
+    ydata : numpy.ndarray
+    nbins_x : int
+        Number of bins along x dimension
+    nbins_y : int
+        Number of bins along y dimension
+    ax : matplotlib.Axes (optional)
+        If supplied, plot the contour to this axis. Otherwise, open a new figure
+    contour_kwargs : dict
+        kwargs to be passed to pyplot.contour()
+        
+    Example Usage
+    -------------
+     density_contour(x pos, y pos, contour res, contour res, axis, colors for contours)
+     e.g.:
+     density_contour(xD, yD, 80, 80, ax=ax, 
+         colors=['red','orange', 'yellow', 'orange', 'yellow'])
+
+    """
+
+    H, xedges, yedges = np.histogram2d(xdata, ydata, bins=(nbins_x,nbins_y), density=True)
+    # NOTE : if you are using the latest version of python, in the above: 
+    # instead of normed=True, use density=True
+    
+    x_bin_sizes = (xedges[1:] - xedges[:-1]).reshape((1,nbins_x))
+    y_bin_sizes = (yedges[1:] - yedges[:-1]).reshape((nbins_y,1))
+
+    pdf = (H*(x_bin_sizes*y_bin_sizes))
+    
+    X, Y = 0.5*(xedges[1:]+xedges[:-1]), 0.5*(yedges[1:]+yedges[:-1])
+    Z = pdf.T
+    fmt = {}
+    
+    ### Adjust Here #### 
+    
+    # Contour Levels Definitions
+    one_sigma = so.brentq(find_confidence_interval, 0., 1., args=(pdf, 0.68))
+    two_sigma = so.brentq(find_confidence_interval, 0., 1., args=(pdf, 0.95))
+    three_sigma = so.brentq(find_confidence_interval, 0., 1., args=(pdf, 0.99))
+    
+    # You might need to add a few levels
+
+    # Array of Contour levels. Adjust according to the above
+    levels = [one_sigma, two_sigma, three_sigma][::-1]
+    
+    # contour level labels  Adjust accoding to the above.
+    strs = ['0.68', '0.95', '0.99'][::-1]
+
+    
+    ###### 
+    
+    if ax == None:
+        contour = plt.contour(X, Y, Z, levels=levels, origin="lower", **contour_kwargs)
+        for l, s in zip(contour.levels, strs):
+            fmt[l] = s
+        plt.clabel(contour, contour.levels, inline=True, fmt=fmt, fontsize=12)
+
+    else:
+        contour = ax.contour(X, Y, Z, levels=levels, origin="lower", **contour_kwargs)
+        for l, s in zip(contour.levels, strs):
+            fmt[l] = s
+        ax.clabel(contour, contour.levels, inline=True, fmt=fmt, fontsize=12)
+    
+    return contour
